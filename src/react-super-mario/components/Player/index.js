d2c06daefac89195a5c0bb043b2d87e2890d8233
Fábio Paiva <fabio@paiva.info>
2018-04-02 02:07:30 +0200

    Mario jumps

--- src/react-super-mario/components/Player/index.js
+++ src/react-super-mario/components/Player/index.js
@@ -2,23 +2,34 @@
 import React from 'react';
 import mario from '../../assets/players/mario.gif';
 import marioMoving from '../../assets/players/mario-moving.gif';
+import marioJumping from '../../assets/players/mario-jumping.gif';
 import './Player.css';
+import variables from '../../util/variables';
 
 type Props = {
-  position: number,
+  positionX: number,
+  positionY: number,
   direction: 'ltr' | 'rtl',
   isMoving: boolean,
+  isJumping: boolean,
 }
 
 export default class Player extends React.PureComponent<Props> {
    render() {
-    const { direction, position, isMoving } = this.props;
+    const { direction, positionX, positionY, isMoving, isJumping } = this.props;
+    let image = mario;
+    if (isMoving) {
+      image = marioMoving;
+    } else if (isJumping) {
+      image = marioJumping;
+    }
     return <div
       className="Player"
       style={{
-        backgroundImage: `url(${isMoving ? marioMoving : mario})`,
+        backgroundImage: `url(${image})`,
         transform: direction === 'ltr' ? 'none' : 'scaleX(-1)',
-        left: `${position}px`,
+        left: `${positionX}px`,
+        bottom: `${positionY + variables.groundHeight}px`,
       }}
     />
   }
--- src/react-super-mario/index.js
+++ src/react-super-mario/index.js
@@ -4,33 +4,40 @@ import Background from './components/Background';
 import Mario from './components/Player';
 import allowedKeys from './util/allowedKeys';
 import variables from './util/variables';
-import introMusic from './assets/musics/mpi.wav';
-import music from './assets/musics/mp.wav';
+import introMusic from './assets/audio/mpi.wav';
+import music from './assets/audio/mp.wav';
+import jumpAudio from './assets/audio/smw_jump.wav';
 import './ReactSuperMario.css';
 
 type Props = {}
 
 type State = {
   width?: number,
-  position: number,
+  positionX: number,
+  positionY: number,
   scenarioPosition: number,
   direction: 'ltr' | 'rtl',
   isMoving: boolean,
+  isJumping: boolean,
+  isFalling: boolean,
 }
 
 export default class ReactSuperMario extends React.Component<Props, State> {
   state = {
-    position: 0,
+    width: 0,
+    positionX: 0,
+    positionY: 0,
     scenarioPosition: 0,
     direction: 'ltr',
     isMoving: false,
-    width: 0,
+    isJumping: false,
+    isFalling: false,
   }
 
   componentDidMount() {
     window.addEventListener('keydown', this.keyDown)
     window.addEventListener('keyup', this.keyUp)
-    this._gameCoreRunTimeout = setInterval(this.gameCoreRun, 50)
+    this._gameCoreRunTimeout = setInterval(this.gameCoreRun, 80)
   }
 
   componentWillUnmount() {
@@ -43,29 +50,54 @@ export default class ReactSuperMario extends React.Component<Props, State> {
   _activeKeys: typeof allowedKeys = allowedKeys;
   _gameContainerRef: ?HTMLDivElement;
   _audioRef: ?HTMLAudioElement;
+  _audioSfxRef: ?HTMLAudioElement;
 
   gameCoreRun = () => {
-    const { direction, position, width, scenarioPosition } = this.state;
+    const { direction, positionX, positionY, width, scenarioPosition, isJumping, isFalling } = this.state;
     const { _activeKeys } = this;
     const step = _activeKeys.a || _activeKeys.s ?  20 : 10;
+    const jumpLimit = 120;
+
+    // Movement
     if (_activeKeys.ArrowRight ? !_activeKeys.ArrowLeft : _activeKeys.ArrowLeft) {
-      this.setState({ isMoving: true })
+      this.setState({ isMoving: true });
       if (direction === 'ltr' && _activeKeys.ArrowLeft) {
-        this.setState({ direction: 'rtl' })
+        this.setState({ direction: 'rtl' });
       } else if (direction === 'rtl' && _activeKeys.ArrowRight) {
-        this.setState({ direction: 'ltr' })
+        this.setState({ direction: 'ltr' });
       }
-      if (_activeKeys.ArrowRight && width && position > (width * 40 / 100)) {
-        this.setState({ scenarioPosition: scenarioPosition + step })
-      } else if (_activeKeys.ArrowLeft && position <= step && scenarioPosition > 0) {
-        this.setState({ scenarioPosition: scenarioPosition - step })
-      } else if (_activeKeys.ArrowRight && width && position < (width - variables.marioWidth - step)) {
-        this.setState({ position: position + step })
-      } else if (_activeKeys.ArrowLeft && position > 0) {
-        this.setState({ position: position - step })
+      if (_activeKeys.ArrowRight && width && positionX > (width * 40 / 100)) {
+        this.setState({ scenarioPosition: scenarioPosition + step });
+      } else if (_activeKeys.ArrowLeft && positionX <= step && scenarioPosition > 0) {
+        this.setState({ scenarioPosition: scenarioPosition - step });
+      } else if (_activeKeys.ArrowRight && width && positionX < (width - variables.marioWidth - step)) {
+        this.setState({ positionX: positionX + step });
+      } else if (_activeKeys.ArrowLeft && positionX > 0) {
+        this.setState({ positionX: positionX - step });
       }
     } else {
-      this.setState({ isMoving: false })
+      this.setState({ isMoving: false });
+    }
+
+    // Jump
+    if (_activeKeys.z && !isFalling) {
+      if (!isJumping) {
+        this._audioSfxRef.src = jumpAudio;
+        this._audioSfxRef.play();
+      }
+      this.setState({ isJumping: true });
+      if (positionY < jumpLimit) {
+        this.setState({ positionY: positionY + 30 });
+      } else {
+        this.setState({ isFalling: true });
+      }
+    } else {
+      if (positionY > 0) {
+        const nextPositionY = positionY - (_activeKeys.z ? 30 : 40)
+        this.setState({ isFalling: true, positionY: nextPositionY >= 0 ? nextPositionY : 0 });
+      } else {
+        this.setState({ isJumping: false, isFalling: _activeKeys.z });
+      }
     }
   }
 
@@ -89,6 +121,12 @@ export default class ReactSuperMario extends React.Component<Props, State> {
     }
   }
 
+  getSfxAudioRef = (ref: ?HTMLAudioElement) => {
+    if (ref) {
+      this._audioSfxRef = ref;
+    }
+  }
+
   keyDown = (event: KeyboardEvent) => {
     const { _activeKeys } = this;
     if (_activeKeys[event.key] === false ) {
@@ -105,16 +143,25 @@ export default class ReactSuperMario extends React.Component<Props, State> {
 
   render() {
     const { 
-      position, 
+      positionX, 
+      positionY,
       direction, 
       isMoving,
+      isJumping,
       scenarioPosition,
     } = this.state
     return (
       <div className="ReactSuperMario" ref={this.getRef}>
         <Background position={scenarioPosition} />
-        <Mario position={position} direction={direction} isMoving={isMoving} />
+        <Mario 
+          positionX={positionX} 
+          positionY={positionY} 
+          direction={direction} 
+          isMoving={isMoving} 
+          isJumping={isJumping} 
+        />
         <audio src={introMusic} ref={this.getAudioRef} />
+        <audio ref={this.getSfxAudioRef} />
       </div>
     )
   }
